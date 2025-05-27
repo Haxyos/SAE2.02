01e194117e0c2b0cd60bdf494ae9ac05cb50ea54
Mylan <mylan.baudin-marie@etu.unicaen.fr>
2025-05-27 09:50:32 +0200

    isConnexe() fini

--- SAE2.01J2/src/main/java/app/ai/WorldAnalyzer.java
+++ SAE2.01J2/src/main/java/app/ai/WorldAnalyzer.java
@@ -1,7 +1,9 @@
 package app.ai;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import app.model.map.Place;
 import app.model.map.World;
@@ -13,11 +15,26 @@ public class WorldAnalyzer {
 	public boolean isConnexe() {
 		if (world.getPlaces().size() == 0) return true;
 		
+		Set<Place> visited = new HashSet();
+		dfs(world.getPlaces().get(0),visited);
 		
+		if ( visited.size() == world.getPlaces().size()) {
+			return true;
+		} else{
+			return false;
+		}
+	}
+	
+	private void dfs(Place current, Set<Place> visited) {
+		if (visited.contains(current)) return;
+		visited.add(current);
+		for (Place neighbor : current.getPaths().keySet()) {
+			dfs(neighbor, visited);
+		}
 	}
 	
 	public boolean isFinishable() {
-		
+		return true;
 	}
 	
 	/* Bonus :
