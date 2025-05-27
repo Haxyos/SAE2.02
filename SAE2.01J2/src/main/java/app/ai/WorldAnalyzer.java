e734d67d96f9cfc5b4071cc0f62afd2c86b3d90a
Mylan <mylan.baudin-marie@etu.unicaen.fr>
2025-05-27 16:49:40 +0200

    Ajout de la méthode lessDistanceToReach + plusieurs tests

--- SAE2.01J2/src/main/java/app/ai/WorldAnalyzer.java
+++ SAE2.01J2/src/main/java/app/ai/WorldAnalyzer.java
@@ -95,7 +95,7 @@ public class WorldAnalyzer {
 	 
 	 */
 	 
-	List<Map<Place,Integer>>dijkstraWithSteps(Place start){
+	public List<Map<Place,Integer>>dijkstraWithSteps(Place start){
 		List<Map<Place,Integer>> steps = new ArrayList<>();
 		Map<Place,Integer> distances = new HashMap<>();
 		Set<Place> visited = new HashSet<>();
@@ -149,7 +149,15 @@ public class WorldAnalyzer {
 	}
 		
 	public Integer lessDistanceToReach(Place placeFromId, Place placeFromId2) {
-		return null;
+		List<Map<Place, Integer>> steps = dijkstraWithSteps(placeFromId);
+	    int result = Integer.MAX_VALUE;
+
+	    for (Map<Place, Integer> step : steps) {
+	        if (step.containsKey(placeFromId2)) {
+	            result = step.get(placeFromId2);
+	        }
+	    }
+
+	    return result;
 	}
-	
 }
--- SAE2.01J2/src/test/java/app/controller/WorldAnalyzerTest.java
+++ SAE2.01J2/src/test/java/app/controller/WorldAnalyzerTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -16,20 +18,32 @@ public class WorldAnalyzerTest {
 
 	World w;
     WorldAnalyzer analyzer;
+    World w2;
+    WorldAnalyzer analyzer2;
 
     @BeforeEach
     public void setUp() throws Exception {
         w = WorldIO.loadWorld(Main.class.getResourceAsStream("Monde1.json"));
         analyzer = new WorldAnalyzer(w);
+        w2 = WorldIO.loadWorld(Main.class.getResourceAsStream("MondeTest1.json"));
+        analyzer2 = new WorldAnalyzer(w2);
     }
 
     @Test
     public void testConnex() {
         assertTrue(analyzer.isConnexe());
+        assertFalse(analyzer2.isConnexe());
     }
-
+    
+    @Test
+    public void testFinish() {
+    	assertTrue(analyzer.isFinishable());
+    	assertFalse(analyzer2.isFinishable());
+    }
+    
     @Test
     public void testLessDistanceToReach() {
         assertEquals(12, analyzer.lessDistanceToReach(w.getPlaceFromId(1), w.getPlaceFromId(6)));
+        assertEquals(9, analyzer.lessDistanceToReach(w.getPlaceFromId(1), w.getPlaceFromId(9)));
     }
 }
